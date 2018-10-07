4877bcf9d4041cd6cb44535d5967d1b7354cfb74
Manuel Juan Fosela Águila <manufosela@gmail.com>
2018-10-07 22:11:46 +0200

    refactor with lit-element

--- circle-percent.js
+++ circle-percent.js
@@ -1,4 +1,5 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { LitElement, html } from '/node_modules/@polymer/lit-element/lit-element.js';
+import {afterNextRender, beforeNextRender} from '/node_modules/@polymer/polymer/lib/utils/render-status.js';
 
 /**
  * `circle-alone`
@@ -8,23 +9,27 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
  * @polymer
  * @demo
  */
-class CircleAlone extends PolymerElement {
-  static get template() {
-    return html`
-      <svg>
-        <circle cx="50" cy="50" r="47" stroke-dasharray$="[[dasharray]]" stroke="#cb2240" stroke-width="4" fill="none" />
-      </svg>
-    `;
-  }
+class CircleAlone extends LitElement {
   static get properties() {
     return {
-      dasharray: {
-        type:  String,
-        value: '225, 300',
-      },
-    };
+      dasharray: { type:  String },
+    }
+  }
+
+  constructor() {
+    super();
+    this.dasharray = "225, 300";
+  }
+
+  render() {
+    return html`
+    <svg>
+      <circle cx="50" cy="50" r="47" stroke-dasharray="${this.dasharray}" stroke="#cb2240" stroke-width="4" fill="none" />
+    </svg>
+    `;
   }
 }
+
 window.customElements.define('circle-alone', CircleAlone);
 
 
@@ -36,47 +41,66 @@ window.customElements.define('circle-alone', CircleAlone);
  * @polymer
  * @demo demo/index.html
  */
-class CirclePercent extends PolymerElement {
-  static get template() {
+class CirclePercent extends LitElement {
+  static get properties() {
+    return {
+      title: { type: String },
+      percent: { type: Number },
+      dasharray: { type: String },
+      _radio: { type: Number },
+      diametro:{ type: Number }
+    }
+  }
+
+  constructor() {
+    super();
+    this.title = "TITLE";
+    this.percent = 25;
+    this.dasharray = '' + 300 * this.percent / 100 + ' 300';
+    this._radio = 100;
+    this.diametro = this._radio * 2;
+  }
+
+  render() {
     return html`
-      <style>
-        .product-inf {
-          text-align: center;
+    <style>
+      .circle-inf {
+        text-align: center;
+      }
+      @media screen and (max-width: 767px) {
+        .circle-inf {
+          width: 100%;
         }
-        @media screen and (max-width: 767px) {
-          .product-inf {
-            width: 100%;
-          }
-        }
-        .product-inf__percent {
-          font-weight: 700;
-          font-size: 22px;
-          letter-spacing: 1px;
-          margin-bottom: 12px;
-          font-family: "Dosis", sans-serif;
-          position: relative;
-        }
-        .product-inf__percent circle {
-          transform: rotate(180deg) scaleY(-1);
-          transform-origin: 50%;
-        }
-        .product-inf__percent-txt {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-        }
-        .product-inf__title {
-          font-family: "Dosis", sans-serif;
-          font-weight: 700;
-          letter-spacing: 2px;
-          font-size: 18px;
-        }
-        
-      </style>
-      <div class="product-inf">
-      <div class="product-inf__percent">
-        <div class="product-inf__percent-circle">
+      }
+      .circle-inf__percent {
+        font-weight: 700;
+        font-size: 22px;
+        letter-spacing: 1px;
+        margin-bottom: 12px;
+        font-family: "Dosis", sans-serif;
+        position: relative;
+      }
+      .circle-inf__percent circle {
+        transform: rotate(180deg) scaleY(-1);
+        transform-origin: 50%;
+      }
+      .circle-inf__percent-txt {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+      .circle-inf__title {
+        font-family: "Dosis", sans-serif;
+        font-weight: 700;
+        letter-spacing: 2px;
+        font-size: 18px;
+      }
+      
+    </style>
+    <div class="circle-inf">
+      <div class="circle-inf__percent">
+        <div class="circle-inf__percent-circle">
           <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
             <defs>
               <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
@@ -85,51 +109,17 @@ class CirclePercent extends PolymerElement {
               </linearGradient>
             </defs>
             <foreignObject>
-              <circle-alone dasharray="[[dasharray]]"></circle-alone>
+              <circle-alone dasharray="${this.dasharray}"></circle-alone>
             </foreignObject>
           </svg>
         </div>
-        <div class="product-inf__percent-txt">
-          [[percent]]%
+        <div class="circle-inf__percent-txt">
+          ${this.percent}%
         </div>
       </div>
-      <span class="product-inf__title">[[title]]</span>
+      <span class="circle-inf__title">${this.title}</span>
+    </div>
     `;
-  }
-  static get properties() {
-    return {
-      title: {
-        type: String,
-        value: 'TITLE',
-      },
-      percent: {
-        type: Number,
-        value: 25,
-        observer:'_percentChange'
-      },
-      dasharray: {
-        type: String,
-        value: '75 300'
-      },
-      _radio: {
-        type: Number,
-        value:100,
-      },
-      diametro:{
-        type: Number,
-        computed: 'getDiametro(_radio)',
-      }
-    };
-  }
-
-  getDiametro(_radio) {
-    return _radio*2;
-  }
-
-  _percentChange() {
-    this.dasharray =  '' + 300 * this.percent / 100 + ' 300';
-    console.log(a);
-    return a;
   }
 }
 
